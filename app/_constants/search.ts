19c40312a41a00de4ffb6bb009f0dadbef314d59
lucasmingatos <lucasmingatos@hotmail.com>
2024-08-07 11:26:06 -0300

    feat: add BookingItem component

---
+++ app/_constants/search.ts
@@ -0,0 +1,24 @@
+interface QuickSearchOptions {
+    imageUrl: string;
+    title: string;
+}
+
+export const quickSearchOptions: QuickSearchOptions[] = [{
+    imageUrl: "/acabamento.svg",
+    title: "Acabamento"
+}, {
+    imageUrl: "/barba.svg",
+    title: "Barba"
+}, {
+    imageUrl: "/cabelo.svg",
+    title: "Cabelo"
+}, {
+    imageUrl: "/hidratacao.svg",
+    title: "Hidratação"
+}, {
+    imageUrl: "/massagem.svg",
+    title: "Massagem"
+}, {
+    imageUrl: "/sobrancelha.svg",
+    title: "Sobrancelha"
+}]
--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,22 @@ import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
 import { Card, CardContent } from "./_components/ui/card"
-import { Badge } from "./_components/ui/badge"
-import { Avatar, AvatarImage } from "./_components/ui/avatar"
 import { db } from "./_lib/prisma"
 import BarbershopItem from "./_components/barbershop-item"
+import { quickSearchOptions } from "./_constants/search"
+import BookingItem from "./_components/booking-item"
+
+//RECEBER AGENDAMENTOS COMO PROP FUTURAMENTE
 
 const Home = async () => {
 
   //instanciar banco
   const barbershops = await db.barbershop.findMany({})
-  console.log({ barbershops })
+  const pouparBarbershops = await db.barbershop.findMany({
+    orderBy: {
+      name: "desc",
+    }
+  })
 
   return (
     <div>
@@ -32,6 +38,20 @@ const Home = async () => {
           </Button>
         </div>
 
+        {/*busca rápida*/}
+        <div className="flex gap-3 mt-6 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+          {quickSearchOptions.map((option) => (
+            <Button className="gap-3" variant="secondary" key={option.title}>
+              <Image
+                alt={option.title}
+                src={option.imageUrl}
+                width={16}
+                height={16} />
+              {option.title}
+            </Button>
+          ))}
+        </div>
+
         {/*imagem*/}
         <div className="relative mt-6 h-[150px] w-full">
           <Image
@@ -41,37 +61,38 @@ const Home = async () => {
             className="rounded-xl object-cover" />
         </div>
 
-        {/*agendamento*/}
-        <h2 className="m-3 mt-6 uppercase text-gray-400 font-bold text-xs">Agendamentos</h2>
-        <Card>
-          <CardContent className="flex justify-between p-0">
-            {/*esquerda*/}
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3 className="font-semibold">Corte de cabelo</h3>
-              <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"></AvatarImage>
-                </Avatar>
-                <p className="text-sm">Barbearia do Lu</p>
-              </div>
-            </div>
-            {/*direita*/}
-            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-              <p className="text-sm">Agosto</p>
-              <p className="text-2xl">05</p>
-              <p className="text-sm">20:00</p>
-            </div>
-          </CardContent>
-        </Card>
+        {/*agendamentos*/}
+        <BookingItem />
 
+        {/*recomendados*/}
         <h2 className="m-3 mt-6 uppercase text-gray-400 font-bold text-xs">Recomendados</h2>
         <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
           {barbershops.map((barbershop) => (
             <BarbershopItem key={barbershop.id} barbershop={barbershop} />
           ))}
         </div>
+
+        {/*populares*/}
+        <h2 className="m-3 mt-6 uppercase text-gray-400 font-bold text-xs">Populares</h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {pouparBarbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+
+
       </div>
+
+      {/*populares*/}
+      <footer>
+        <Card className="px-5 py-6">
+          <CardContent>
+            <p className="text-sm text-gray-400">
+              ® 2024 Copyright <span className="font-bold">Lucas Mingatos</span>
+            </p>
+          </CardContent>
+        </Card>
+      </footer>
     </div >
   )
 }
