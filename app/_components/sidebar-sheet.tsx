059e915ed6e44189b83f3af48f231c5d92ca737c
lucasmingatos <lucasmingatos@hotmail.com>
2024-08-09 11:54:41 -0300

    chore: add next auth setup

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
-import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react";
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react";
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet";
 import { quickSearchOptions } from "../_constants/search";
-import { Avatar, AvatarImage } from "./ui/avatar";
 import Link from "next/link";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog";
 
 const SidebarSheet = () => {
     return (
@@ -14,7 +14,38 @@ const SidebarSheet = () => {
                 <SheetTitle className="text-left">Menu</SheetTitle>
             </SheetHeader>
 
-            <div className="py-5 border-b border-solid flex items-center gap-3">
+            <div className="py-5 border-b border-solid flex items-center gap-3 justify-between">
+                <h2 className="font-bold">Olá, faça seu login.</h2>
+                <Dialog>
+
+                    <DialogTrigger asChild>
+                        <Button size="icon">
+                            <LogInIcon />
+                        </Button>
+                    </DialogTrigger>
+
+                    <DialogContent className="w-[90%]">
+                        <DialogHeader>
+                            <DialogTitle>Faça login na plataforma</DialogTitle>
+                            <DialogDescription>
+                                Conect-se usando sua conta do Google.
+                            </DialogDescription>
+                        </DialogHeader>
+
+                        <Button variant="outline" className="gap-1 font-bold">
+                            <Image
+                                alt="Fazer login com google"
+                                src="/google.svg"
+                                width={18}
+                                height={18}
+                            />
+                            Google
+                        </Button>
+
+                    </DialogContent>
+                </Dialog>
+
+                {/**
                 <Avatar>
                     <AvatarImage src="https://ionicframework.com/docs/img/demos/avatar.svg"></AvatarImage>
                 </Avatar>
@@ -23,6 +54,7 @@ const SidebarSheet = () => {
                     <p className="font-bold">Lucas Mingatos</p>
                     <p className="text-sm">lucasmingatos@hotmail.com</p>
                 </div>
+                 */}
             </div>
 
             <div className="py-5 flex flex-col gap-2 border-b border-solid">
